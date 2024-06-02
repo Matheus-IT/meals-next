d9273569a07ab6c8a8f2595f3d82681ac2de32e7
matheus <matheuscosta.tech@gmail.com>
2024-06-01 21:49:01 -0300

    Adding dummy badges

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
+// import { useEffect, useState } from "react";
 
 export default function Home() {
+  // const [recipes, setRecipes] = useState([]);
+
+  // useEffect(() => {
+  //   console.log("useEffect");
+
+  //   async function getRecipes() {
+  //     const res = await fetch("https://dummyjson.com/recipes");
+  //     const data = await res.json();
+  //     if (data) {
+  //       console.log("data", data);
+
+  //       // setRecipes(data);
+  //     }
+  //   }
+
+  //   getRecipes();
+  // }, []);
+
+  const cuisines: string[] = ["b1", "b2", "b3"];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Test</Button>
-    </main>
+    <div>
+      {cuisines.map((cuisine, index) => (
+        <Badge
+          key={`${cuisine}-${index}`}
+          variant="outline"
+          className="border-orange-800 text-gray-900 text-lg mx-2 my-1 hover:cursor-pointer bg-orange-50 hover:scale-110 ease-in duration-200"
+        >
+          {cuisine}
+        </Badge>
+      ))}
+    </div>
   );
 }

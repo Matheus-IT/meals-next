96efc24f039107ef8b53a4477f317175c71981da
matheus <matheuscosta.tech@gmail.com>
2024-06-01 22:11:13 -0300

    Adding dummy card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 // import { useEffect, useState } from "react";
 
@@ -23,6 +31,14 @@ export default function Home() {
   // }, []);
 
   const cuisines: string[] = ["b1", "b2", "b3"];
+  const recipes = [
+    {
+      name: "one",
+      servings: 2,
+      prepTimeMinutes: 5,
+      cookTimeMinutes: 5,
+    },
+  ];
 
   return (
     <div>
@@ -30,10 +46,36 @@ export default function Home() {
         <Badge
           key={`${cuisine}-${index}`}
           variant="outline"
-          className="border-orange-800 text-gray-900 text-lg mx-2 my-1 hover:cursor-pointer bg-orange-50 hover:scale-110 ease-in duration-200"
+          className="border-orange-800 text-gray-900 text-lg mx-2 my-1 hover:cursor-pointer bg-orange-50 hover:scale-110 ease-in-out duration-200"
         >
           {cuisine}
         </Badge>
+      ))}
+
+      {recipes.map((recipe, index) => (
+        <Card
+          key={`${recipe.name}-${index}`}
+          className="flex flex-col bg-orange-50 hover:scale-105 ease-in-out duration-200 xl:min-h-[600px] fancyGradient"
+        >
+          <CardHeader>{/* image */}</CardHeader>
+          <CardContent className="uppercase lg:text-3xl relative font-bold line-clamp-2">
+            <p>{recipe.name}</p>
+          </CardContent>
+          <CardFooter className="flex items-start gap-2 lg:gap-12 lg:flex-row flex-col">
+            <div className="flex flex-col">
+              <p className="text-lg">Serves</p>
+              <p className="text-gray-800">{recipe.servings}</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="text-lg">Prep Time</p>
+              <p className="text-gray-800">{recipe.prepTimeMinutes}m</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="text-lg">Cook Time</p>
+              <p className="text-gray-800">{recipe.cookTimeMinutes}m</p>
+            </div>
+          </CardFooter>
+        </Card>
       ))}
     </div>
   );

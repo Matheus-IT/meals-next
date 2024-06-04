ecd4d6539242afc46059756a2b1ae92a0c51e642
matheus <matheuscosta.tech@gmail.com>
2024-06-03 23:08:42 -0300

    Using custom fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Lilita_One } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/ui/nav-bar";
 import RecipeProvider from "@/context/recipe-context";
 
-const inter = Inter({ subsets: ["latin"] });
+const lilitaOne = Lilita_One({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={lilitaOne.className}>
         <RecipeProvider>
           <>
             <NavBar />
